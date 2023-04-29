28a487468cf1fe1bde207c340b71aebd48e29b65
DS <dsrathorebsw1234@gmail.com>
2023-04-29 12:55:34 +0530

    Create Config File

--- frontend/firebaseConfig.js
+++ frontend/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAtrGGUvuLXtAOZinmjOKBr_gA0HrFsX50",
-    authDomain: "arf-auth.firebaseapp.com",
-    projectId: "arf-auth",
-    storageBucket: "arf-auth.appspot.com",
-    messagingSenderId: "812159886339",
-    appId: "1:812159886339:web:be38e146a4495a92ffda36",
-    measurementId: "G-275MXLZX3B"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
